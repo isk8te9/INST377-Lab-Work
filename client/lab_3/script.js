be91af9a9f058810430c3dfc856a453252be326e
snbeasley <42461309+isk8te9@users.noreply.github.com>
2023-02-19 17:39:41 -0500

    lab 3 changes in progress

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -7,10 +7,10 @@
 */
 /* eslint-enable max-len */
 // set our first slide's position to "0", the opening position in an array
-const slidePosition = 0;
+let slidePosition = 0;
 
 // gather a reference to every slide we're using via the class name and querySelectorAll
-const slides = document.querySelectorAll('.carousel_item');
+const slides = document.querySelectorAll(".carousel_item");
 
 // change that "NodeList" into a Javascript "array", to get access to "array methods"
 const slidesArray = Array.from(slides);
@@ -19,13 +19,12 @@ const slidesArray = Array.from(slides);
 const totalSlides = slidesArray.length;
 
 function updateSlidePosition() {
-  // Using the .forEach array method, (array.forEach((element) => { per-element work goes here }))
-  // loop through all the slides in your slideArray
-  // and remove the 'visible' class from each classList
-  // then add a class 'hidden' to all of them
+  slidesArray.forEach((slide) => {
+    slide.classList.remove("visible");
+    slide.classList.add("hidden");
+  });
 
-  // outside your .forEach,
-  // add a 'visible' class to the slide at the current slidePosition in slides
+  slides[slidePosition].classList.add('visible');
 }
 
 function moveToNextSlide() {
@@ -52,11 +51,18 @@ function moveToPrevSlide() {
   These two functions have been assigned via "addEventListener"
   to the elements accessed by the "querySelector" set to the class name on each
 */
-document.querySelector('.next') // Get the appropriate element (<button class="next">)
-  .addEventListener('click', () => { // set an event listener on it - when it's clicked, do this callback function
-    console.log('clicked next'); // let's tell the client console we made it to this point in the script
+document
+  .querySelector(".next") // Get the appropriate element (<button class="next">)
+  .addEventListener("click", () => {
+    // set an event listener on it - when it's clicked, do this callback function
+    console.log("clicked next"); // let's tell the client console we made it to this point in the script
     moveToNextSlide(); // call the function above to handle this
   });
+
+document.querySelector(".prev").addEventListener("click", () => {
+  console.log("clicked prev");
+  moveToPrevSlide();
+});
 
 // Paying close attention to the above queryselector, write one that fires
 // when you want a "prev" slide
