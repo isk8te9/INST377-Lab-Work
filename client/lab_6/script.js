d241d883606b4ca291467dfe2af3031224232875
snbeasley <42461309+isk8te9@users.noreply.github.com>
2023-03-19 23:54:00 -0400

    Lab 6 (finished)

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,35 +1,37 @@
 /* eslint-disable max-len */
 
-/*
-  Hook this script to index.html
-  by adding `<script src="script.js">` just before your closing `</body>` tag
-*/
 
-/*
-  ## Utility Functions
-    Under this comment place any utility functions you need - like an inclusive random number selector
-    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-*/
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
 
-
 function injectHTML(list) {
-  console.log('fired injectHTML');
+  console.log("fired injectHTML");
   const target = document.querySelector('#restaurant_list');
   target.innerHTML = '';
   list.forEach((item) => {
     const str = `<li>${item.name}</li>`;
-    target.innerHTML += str
-  })
+    target.innerHTML += str;
+  });
+}
 
+function filterList(list, query) {
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  });
 }
 
 function processRestaurants(list) {
-  console.log('fired restaurants list');
+  console.log("fired restaurants list");
+  const range = [...Array(15).keys()];
+  return (newArray = range.map((item) => {
+    const index = getRandomIntInclusive(0, list.length - 1);
+    return list[index];
+  }));
 
   /*
     ## Process Data Separately From Injecting It
@@ -52,56 +54,65 @@ function processRestaurants(list) {
 }
 
 async function mainEvent() {
-  /*
-    ## Main Event
-      Separating your main programming from your side functions will help you organize your thoughts
-      When you're not working in a heavily-commented "learning" file, this also is more legible
-      If you separate your work, when one piece is complete, you can save it and trust it
-  */
-
   // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
-  const submit = document.querySelector('button[type="submit"]'); // get a reference to your submit button
-  submit.style.display = 'none'; // let your submit button disappear
+  const form = document.querySelector(".main_form"); // This class name needs to be set on your form before you can listen for an event on it
+  const filterButton = document.querySelector("#filter");
+  const loadDataButton = document.querySelector("#data_load");
+  const generateListButton = document.querySelector("#generate");
 
-  /*
-    Let's get some data from the API - it will take a second or two to load
-    
-   */
-  const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
-  const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
+  const loadAnimation = document.querySelector('#data_load_animation');
+  loadAnimation.style.display = 'none'
 
-  /*
-    Below this comment, we log out a table of all the results:
-  */
-  console.table(arrayFromJson);
+  let currentList = [];
 
-  // As a next step, log the first entry from your returned array of data.
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
-  console.log(`replace me with the first entry`);
+  loadDataButton.addEventListener("click", async (submitEvent) => {
+    // async has to be declared on every function that needs to "await" something
+    console.log("Loading Data"); // this is substituting for a "breakpoint"
+    loadAnimation.style.display = 'inline-block';
 
-  // Now write a log using string interpolation - log out the name and category of your first returned entry (index [0]) to the browser console
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Property_accessors
-  console.log(`replace me with the name and category of the first entry`);
+    const results = await fetch(
+      "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"
+    );
 
-  // This IF statement ensures we can't do anything if we don't have information yet
-  if (arrayFromJson?.length > 0) { // the question mark in this means "if this is set at all"
-    submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
+    /*
+   ## Get request with query parameters
 
-    // And here's an eventListener! It's listening for a "submit" button specifically being clicked
-    // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
-    form.addEventListener('submit', (submitEvent) => {
-      // Using .preventDefault, stop the page from refreshing when a submit event happens
-      // https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+      const results = await fetch(`/api/foodServicePG?${new URLSearchParams(formProps)}`);
 
-      // This constant will contain the value of your 15-restaurant collection when it processes
-      const restaurantList = processRestaurants(arrayFromJson);
+      The above request uses "string interpolation" to include an encoded version of your form values
+      It works because it has a ? in the string
+      Replace line 37 with it, and try it with a / instead to see what your server console says
 
-      // And this function call will perform the "side effect" of injecting the HTML list for you
-      injectHTML(restaurantList);
-    });
-  }
+      You can check what you sent to your server in your GET request
+      By opening the "network" tab in your browser developer tools and looking at the "name" column
+      This will also show you how long it takes a request to resolve
+    */
+
+    // This changes the response from the GET into data we can use - an "object"
+    currentList = await results.json();
+    loadAnimation.style.display = 'none'
+    console.table(currentList);
+    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+    // it initially contains all 1,000 records from your request
+  });
+
+  filterButton.addEventListener("click", (event) => {
+    console.log("clicked FilterButton");
+
+    const formData = new FormData(form);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.resto);
+
+    console.log(newList);
+  });
+
+  generateListButton.addEventListener("click", (event) => {
+    console.log("generate new list");
+    const restaurantsList = processRestaurants(currentList);
+    injectHTML(restaurantsList);
+  });
 }
 
 /*
@@ -109,4 +120,4 @@ async function mainEvent() {
   It's calling the 'mainEvent' function at line 57
   It runs first because the listener is set to when your HTML content has loaded
 */
-document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
