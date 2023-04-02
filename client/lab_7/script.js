56c89390e68b3f0ee77ac69a16934b277d5fedd6
snbeasley <42461309+isk8te9@users.noreply.github.com>
2023-04-02 02:15:31 -0400

    Lab 7 (finished)

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,39 +1,38 @@
 /* eslint-disable max-len */
 
-
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-  }
-  
-  function injectHTML(list) {
-    console.log("fired injectHTML");
-    const target = document.querySelector('#restaurant_list');
-    target.innerHTML = '';
-    list.forEach((item) => {
-      const str = `<li>${item.name}</li>`;
-      target.innerHTML += str;
-    });
-  }
-  
-  function filterList(list, query) {
-    return list.filter((item) => {
-      const lowerCaseName = item.name.toLowerCase();
-      const lowerCaseQuery = query.toLowerCase();
-      return lowerCaseName.includes(lowerCaseQuery);
-    });
-  }
-  
-  function processRestaurants(list) {
-    console.log("fired restaurants list");
-    const range = [...Array(15).keys()];
-    return (newArray = range.map((item) => {
-      const index = getRandomIntInclusive(0, list.length - 1);
-      return list[index];
-    }));
-  
-    /*
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
+function injectHTML(list) {
+  console.log("fired injectHTML");
+  const target = document.querySelector("#restaurant_list");
+  target.innerHTML = "";
+  list.forEach((item) => {
+    const str = `<li>${item.name}</li>`;
+    target.innerHTML += str;
+  });
+}
+
+function filterList(list, query) {
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  });
+}
+
+function processRestaurants(list) {
+  console.log("fired restaurants list");
+  const range = [...Array(15).keys()];
+  return (newArray = range.map((item) => {
+    const index = getRandomIntInclusive(0, list.length - 1);
+    return list[index];
+  }));
+
+  /*
       ## Process Data Separately From Injecting It
         This function should accept your 1,000 records
         then select 15 random records
@@ -51,30 +50,33 @@ function getRandomIntInclusive(min, max) {
       - Return only their name, category, and location
       - Return the new list of 15 restaurants so we can work on it separately in the HTML injector
     */
-  }
-  
-  async function mainEvent() {
-    // the async keyword means we can make API requests
-    const form = document.querySelector(".main_form"); // This class name needs to be set on your form before you can listen for an event on it
-    const filterButton = document.querySelector("#filter");
-    const loadDataButton = document.querySelector("#data_load");
-    const generateListButton = document.querySelector("#generate");
-  
-    const loadAnimation = document.querySelector('#data_load_animation');
-    loadAnimation.style.display = 'none'
-  
-    let currentList = [];
-  
-    loadDataButton.addEventListener("click", async (submitEvent) => {
-      // async has to be declared on every function that needs to "await" something
-      console.log("Loading Data"); // this is substituting for a "breakpoint"
-      loadAnimation.style.display = 'inline-block';
-  
-      const results = await fetch(
-        "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"
-      );
-  
-      /*
+}
+
+async function mainEvent() {
+  // the async keyword means we can make API requests
+  const form = document.querySelector(".main_form"); // This class name needs to be set on your form before you can listen for an event on it
+  const filterButton = document.querySelector("#filter");
+  const loadDataButton = document.querySelector("#data_load");
+  const generateListButton = document.querySelector("#generate");
+  const textField = document.querySelector("#resto");
+
+  const loadAnimation = document.querySelector("#data_load_animation");
+  loadAnimation.style.display = "none";
+  generateListButton.classList.add("hidden");
+
+  let storedList = [];
+  let currentList = [];
+
+  loadDataButton.addEventListener("click", async (submitEvent) => {
+    // async has to be declared on every function that needs to "await" something
+    console.log("Loading Data"); // this is substituting for a "breakpoint"
+    loadAnimation.style.display = "inline-block";
+
+    const results = await fetch(
+      "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"
+    );
+
+    /*
      ## Get request with query parameters
   
         const results = await fetch(`/api/foodServicePG?${new URLSearchParams(formProps)}`);
@@ -87,38 +89,49 @@ function getRandomIntInclusive(min, max) {
         By opening the "network" tab in your browser developer tools and looking at the "name" column
         This will also show you how long it takes a request to resolve
       */
-  
-      // This changes the response from the GET into data we can use - an "object"
-      currentList = await results.json();
-      loadAnimation.style.display = 'none'
-      console.table(currentList);
-      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-      // it initially contains all 1,000 records from your request
-    });
-  
-    filterButton.addEventListener("click", (event) => {
-      console.log("clicked FilterButton");
-  
-      const formData = new FormData(form);
-      const formProps = Object.fromEntries(formData);
-  
-      console.log(formProps);
-      const newList = filterList(currentList, formProps.resto);
-  
-      console.log(newList);
-    });
-  
-    generateListButton.addEventListener("click", (event) => {
-      console.log("generate new list");
-      const restaurantsList = processRestaurants(currentList);
-      injectHTML(restaurantsList);
-    });
-  }
-  
-  /*
+
+    // This changes the response from the GET into data we can use - an "object"
+    storedList = await results.json();
+    if (storedList.length > 0) {
+      generateListButton.classList.remove("hidden");
+    }
+    loadAnimation.style.display = "none";
+    console.table(storedList);
+    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+    // it initially contains all 1,000 records from your request
+  });
+
+  filterButton.addEventListener("click", (event) => {
+    console.log("clicked FilterButton");
+
+    const formData = new FormData(form);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.resto);
+
+    console.log(newList);
+    injectHTML(newList);
+  });
+
+  generateListButton.addEventListener("click", (event) => {
+    console.log("generate new list");
+    currentList = processRestaurants(storedList);
+    console.log(currentList);
+    injectHTML(currentList);
+  });
+
+  textField.addEventListener("input", (event) => {
+    console.log("input", event.target.value);
+    const newList = filterList(currentList, event.target.value);
+    console.log(newList);
+    injectHTML(newList);
+  });
+}
+
+/*
     This last line actually runs first!
     It's calling the 'mainEvent' function at line 57
     It runs first because the listener is set to when your HTML content has loaded
   */
-  document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
-  
+document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
